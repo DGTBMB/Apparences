1c24e65ac8326879003c44ac67aeb3acf156c0f7
DGTBMB <138143062+DGTBMB@users.noreply.github.com>
2023-08-08 01:01:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def process_cemetery_image(upload_path):
         return {'status': 'error', 'message': 'Failed to read the image'}
     
 # Perform face detection  --- Cemetery
-    faces = face_cascade.detectMultiScale(img, scaleFactor=1.01, minNeighbors=2)
+    faces = face_cascade.detectMultiScale(img, scaleFactor=1.01, minNeighbors=4)
     prof = profile_cascade.detectMultiScale(img, scaleFactor=1.01, minNeighbors=3)
 
     for x, y, w, h in faces:
