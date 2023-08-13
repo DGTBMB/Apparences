c766eb441c04d0da269979e244c125b0fbe4ea86
DGTBMB <138143062+DGTBMB@users.noreply.github.com>
2023-08-12 20:09:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -123,10 +123,10 @@ def process_cemetery_image(upload_path):
     
 # Perform face detection  --- Cemetery
     faces = face_cascade.detectMultiScale(img, scaleFactor=1.01, minNeighbors=9)
-    prof = profile_cascade.detectMultiScale(img, scaleFactor=1.01, minNeighbors=4)
+    prof = profile_cascade.detectMultiScale(img, scaleFactor=1.01, minSize = (30,30), minNeighbors=4)
 
     for x, y, w, h in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (181, 90, 0), minSize = (30, 30), 8)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (181, 90, 0), 8)
     for x, y, w, h in prof:
         cv2.rectangle(img, (x, y), (x + w, y + h), (181, 90, 0), 8)
     
